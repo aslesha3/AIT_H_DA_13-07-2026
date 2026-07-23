2127998cab6421490e541c84c75045fd929e7054
aslesha3 <asleshachintha9@gmail.com>
2026-07-23 13:17:19 +0530

    added files

---
+++ conditional_assignment.py
@@ -0,0 +1,45 @@
+# string="aslesha"
+# i=0
+# while i<len(string):
+#     if string[i] in "aeiou":
+#         print("*")
+#     else:
+#         print(string[i])
+#     i+=1
+   
+
+
+# for i in range(1,101):
+#     # print(i)
+#     print(input("enter a number: "))
+#     if i%2==0:
+#         print("even number")
+#     else:
+#         print("odd number")
+#     i+=1
+
+
+
+# i=1
+# while i<=100:
+#     print(i)
+#     # print(input("enter a number: ")) ##it shows a number in two ways even and odd
+#     if i%2==0:
+#         print("even")
+#     else:
+#         print("odd")   
+#     i+=1
+    
+
+
+i=2
+while i<=100:
+    # print(i)
+    i=int('enter a number: ')
+    print(i)
+    if i/2==0:
+        print("not prime")
+    else:
+        print("prime")
+    
+    i+=1
---
+++ loops.py
@@ -0,0 +1,22 @@
+#while loop
+x=1
+while x<10:
+     print(x) 
+     x+=1
+
+#for loop
+for i in range(7): #start
+     print(i)
+
+for i in range(7,14):  #start,stop
+     print(i)  
+
+for i in range(7,14,3):  #start,stop,step
+     print(i)
+
+
+# #infinite loop
+# x=1
+# while x<10:
+#     print(x) #loop continues    
+         
